53283b57e5e4a1351117ccb82dcd92a8ff7f10c7
Datacrew-Mahalaxmy <cheetufund@gmail.com>
2026-08-16 11:23:45 +0530

    minor changes -express

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
+import express from "express";
 import { createApp } from "./server/_core/app";
 
 const { app } = await createApp();
+
+void express;
 
 export default app;
